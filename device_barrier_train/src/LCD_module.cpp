feb49bf823872920fe095d7354120bfa0d8be7f0
nguyen <duongkhoinguyen.vn@gmail.com>
2026-03-17 12:42:14 +0700

    LCD initiation adjustment

--- device_barrier_train/src/LCD_module.cpp
+++ device_barrier_train/src/LCD_module.cpp
@@ -1,12 +1,12 @@
 #include "LCD_module.h"
 
+LiquidCrystal_I2C lcd(0x27,16,2);
+
 void initI2C() {
     if (Wire.begin(I2C_SDA,I2C_SCL)) {
         Serial.println("I2C LCD successfully connected.");
     }
-
-    LiquidCrystal_I2C lcd(0x27,16,2);
-
     lcd.init();
     lcd.backlight();
+    lcd.clear();
 }
